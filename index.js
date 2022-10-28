943601d8862b998ab259b5079508a09e53ef2558
youngbloodcyb <95453018+youngbloodcyb@users.noreply.github.com>
2022-10-28 13:38:11 -0600

    add viewport size

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const puppeteer = require("puppeteer");
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    await page.setViewport({width: 1920, height: 1080});
     await page.goto("https://www.apple.com/", { waitUntil: "networkidle2" });
 
     await page.screenshot({ path: "screenshot.jpg", 
